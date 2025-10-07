bf96a6704224dd0996f0c4156280c789c691ad29
MDBBee <bobbyugbebor@gmail.com>
2025-10-07 08:07:14 +0300

    Error handling

--- auth/src/routes/signup.ts
+++ auth/src/routes/signup.ts
@@ -1,5 +1,7 @@
-import express, { Request, Response } from 'express';
+import express, { NextFunction, Request, Response } from 'express';
 import { body, validationResult } from 'express-validator';
+import { RequestValidationError } from '../errors/request-validation-error';
+import { DatabaseConnectionError } from '../errors/database-connection-error';
 
 const router = express.Router();
 
@@ -14,16 +16,17 @@ const signUpValidation = [
 router.post(
   '/api/users/signup',
   signUpValidation,
-  (req: Request, res: Response) => {
+  (req: Request, res: Response, next: NextFunction) => {
     const errors = validationResult(req);
 
     if (!errors.isEmpty()) {
-      return res.status(400).send(errors.array());
+      throw new RequestValidationError(errors.array());
     }
 
     const { email, password } = req.body;
     console.log('Creating a user...');
 
+    // throw new DatabaseConnectionError();
     res.send('Hi there-signup-user');
   }
 );
