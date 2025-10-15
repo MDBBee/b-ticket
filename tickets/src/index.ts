1be6201ad92602cb77304a6b756b7972bddd14f9
MDBBee <bobbyugbebor@gmail.com>
2025-10-15 15:19:03 +0300

    created a sigleton class for retrieve a connected instance of a nats-stream

--- tickets/src/index.ts
+++ tickets/src/index.ts
@@ -1,5 +1,6 @@
 import mongoose from 'mongoose';
 import { app } from './app';
+import { natsWrapper } from './nats-wrapper';
 
 const PORT = 3000;
 
@@ -12,6 +13,11 @@ const start = async () => {
   }
 
   try {
+    await natsWrapper.connect(
+      'ticketing',
+      'yueghehg6767',
+      'http://nats-srv:4222'
+    );
     await mongoose.connect(process.env.MONGO_URI);
   } catch (error) {
     console.error(error);
