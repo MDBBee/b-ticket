246c430b5013b1e91138e116158d3141a68a7b69
MDBBee <bobbyugbebor@gmail.com>
2025-10-17 03:05:34 +0300

    Jest test for event publishing ✔

--- tickets/src/index.ts
+++ tickets/src/index.ts
@@ -11,12 +11,21 @@ const start = async () => {
   if (!process.env.MONGO_URI) {
     throw new Error('MONGO_URI env var not found/defined...');
   }
+  if (!process.env.NATS_CLIENT_ID) {
+    throw new Error('NATS_CLIENT_ID env var not found/defined...');
+  }
+  if (!process.env.NATS_CLUSTER_ID) {
+    throw new Error('NATS_CLUSTER_ID env var not found/defined...');
+  }
+  if (!process.env.NATS_URI) {
+    throw new Error('NATS_URI env var not found/defined...');
+  }
 
   try {
     await natsWrapper.connect(
-      'ticketing',
-      'yueghehg6767',
-      'http://nats-srv:4222'
+      process.env.NATS_CLUSTER_ID,
+      process.env.NATS_CLIENT_ID,
+      process.env.NATS_URI
     );
 
     // For gracefull exit of the client-start
