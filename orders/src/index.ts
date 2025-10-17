d505b4b338bd2fa91db73a2ae3051d1546330867
MDBBee <bobbyugbebor@gmail.com>
2025-10-17 03:45:29 +0300

    orders service init

---
+++ orders/src/index.ts
@@ -0,0 +1,51 @@
+import mongoose from 'mongoose';
+import { app } from './app';
+import { natsWrapper } from './nats-wrapper';
+
+const PORT = 3000;
+
+const start = async () => {
+  if (!process.env.JWT_KEY) {
+    throw new Error('JWT_KEY not found/defined...');
+  }
+  if (!process.env.MONGO_URI) {
+    throw new Error('MONGO_URI env var not found/defined...');
+  }
+  if (!process.env.NATS_CLIENT_ID) {
+    throw new Error('NATS_CLIENT_ID env var not found/defined...');
+  }
+  if (!process.env.NATS_CLUSTER_ID) {
+    throw new Error('NATS_CLUSTER_ID env var not found/defined...');
+  }
+  if (!process.env.NATS_URI) {
+    throw new Error('NATS_URI env var not found/defined...');
+  }
+
+  try {
+    await natsWrapper.connect(
+      process.env.NATS_CLUSTER_ID,
+      process.env.NATS_CLIENT_ID,
+      process.env.NATS_URI
+    );
+
+    // For gracefull exit of the client-start
+
+    const client = natsWrapper.client;
+    client.on('close', () => {
+      console.log('Nats connection closed');
+      process.exit();
+    });
+    process.on('SIGINT', () => client.close());
+    process.on('SIGTERM', () => client.close());
+
+    // For gracefull exit of the client-end
+
+    await mongoose.connect(process.env.MONGO_URI);
+  } catch (error) {
+    console.error(error);
+  }
+
+  app.listen(PORT, () => console.log(`Listening on port ${PORT} -- 🐳🐳!!!`));
+};
+
+start();
