825e6db6c0b8d908ac56c6db90cc029e9e9b6429
MDBBee <bobbyugbebor@gmail.com>
2025-10-18 05:57:55 +0300

    jest test for delete-route

---
+++ orders/src/routes/__test__/deleteOrder.test.ts
@@ -0,0 +1,39 @@
+import request from 'supertest';
+import { app } from '../../app';
+import { Order } from '../../models/order';
+import { Ticket } from '../../models/ticket';
+
+it('deletes/cancels an order by updating the order-status', async () => {
+  // create a ticket
+  const ticket = Ticket.createTicket({
+    title: 'show',
+    price: 40,
+  });
+  await ticket.save();
+
+  const user = global.signin();
+  // create an order with the ticket
+  const { body: createdOrder } = await request(app)
+    .post('/api/orders')
+    .set('Cookie', user)
+    .send({ ticketId: ticket.id })
+    .expect(201);
+
+  // cancel/delete/update the order
+  const { body: deletedOrder } = await request(app)
+    .delete(`/api/orders/${createdOrder.id}`)
+    .set('Cookie', user)
+    .expect(204);
+
+  console.log('CreatedORDER', createdOrder);
+
+  //   expect(deletedOrder.status).toEqual('cancelled');
+  const { body: fetchedOrder } = await request(app)
+    .get(`/api/orders/${createdOrder.id}`)
+    .set('Cookie', user)
+    .expect(200);
+
+  expect(fetchedOrder.status).toEqual('cancelled');
+});
+
+it.todo('publishes a cancelled event');
